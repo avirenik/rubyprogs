cb14f23abd759e32adfb8ec1959d1fe0289c733d
Avinash Renikindi <Avinash.Renikindi@copart.com>
2018-03-07 18:14:21 +0530

    modified 5

--- question5.rb
+++ question5.rb
@@ -1,12 +1,12 @@
-a=Matrix[[1,2],[3,4]]
-b=Matrix[[3,4],[2,1]]
-total=Matrix[]
-temp=Matrix[]
-for i in 0..2 do
-  for j in 0..2 do
+a=[[1,2],[3,4]]
+b=[[1,2],[3,4]]
+temp=[]
+total=[]
+a.each do|row| 
+   b.each do|column|
     sum=0
-     for k in 0..2 do
-       sum=sum+a[i][k]*b[k][j]
+      row.each_index do|i|
+       sum=sum+row[i]*column[i]
      end
     temp<<sum
   end
